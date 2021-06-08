25a4f81942199e66f42393ced8fa89e056505b22
jia0_nn <linao264590000@gmail.com>
2021-06-08 13:31:52 +0800

    ROOT: initialize

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+const main = {
+  preload: function () {},
+
+  create: function () {},
+
+  update: function () {},
+};
+
+var game = new Phaser.Game(400, 300, Phaser.AUTO, "gameDiv");
+game.state.add("main", main);
+game.state.start("main");
