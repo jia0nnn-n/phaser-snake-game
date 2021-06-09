a39b77cf3b148490b73a3f3c0ecea5b8d5923ef0
jia0_nn <linao264590000@gmail.com>
2021-06-09 16:27:09 +0800

    make an random apple

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -4,17 +4,36 @@ export default class Snake {
     this.lastMoveTime = 0;
     this.moveInterval = 100;
     this.direction = Phaser.Math.Vector2.RIGHT;
+    this.tileSize = 16;
     this.body = [];
     this.body.push(
-      this.scene.add.rectangle(100, 100, 16, 16, 0xff0000).setOrigin(0)
+      this.scene.add
+        .rectangle(
+          this.scene.game.config.width / 2,
+          this.scene.game.config.height / 2,
+          this.tileSize,
+          this.tileSize,
+          0xff0000
+        )
+        .setOrigin(0)
     );
-    this.body.push(
-      this.scene.add.rectangle(0, 0, 16, 16, 0x0000ff).setOrigin(0)
-    );
-    this.body.push(
-      this.scene.add.rectangle(0, 0, 16, 16, 0xfff).setOrigin(0)
-    );
+    this.apple = this.scene.add
+    .rectangle(0, 0, this.tileSize, this.tileSize, 0x00ff00)
+    .setOrigin(0);
+    this.generateRandomPositionApples()
+
     scene.input.keyboard.on("keydown", (e) => this.handleKeydown(e));
+  }
+
+  generateRandomPositionApples() {
+    this.apple.x =
+      Math.floor(
+        (Math.random() * this.scene.game.config.width) / this.tileSize
+      ) * this.tileSize;
+    this.apple.y =
+      Math.floor(
+        (Math.random() * this.scene.game.config.height) / this.tileSize
+      ) * this.tileSize;
   }
 
   handleKeydown(e) {
